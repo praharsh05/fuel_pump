56fa38cb21822fddc483df19f6cfbe63e34f4200
Praharsh Dubey <praharsh.dubey05@gmail.com>
2021-01-15 21:05:33 +0530

    time based

--- flow_sensor/flow_sensor.ino
+++ flow_sensor/flow_sensor.ino
@@ -5,38 +5,37 @@ Adaptation Courtesy: www.hobbytronics.co.uk
 */
 volatile int flow_frequency; // Measures flow sensor pulses
 unsigned int l_hour; // Calculated litres/hour
-unsigned char flowsensor = 32; // Sensor Input
+unsigned char flowsensor = 21; // Sensor Input
 unsigned long currentTime;
 unsigned long cloopTime;
 int sol=35;
-void flow () // Interrupt function
-{
-   flow_frequency++;
-}
+unsigned long time_lapse;
+int previousValue;
+int currentValue;
+
 void setup()
 {
-   pinMode(flowsensor, INPUT);
+   pinMode(flowsensor, INPUT);   
+   delay(5000);
    pinMode(sol,OUTPUT);
    digitalWrite(sol,HIGH);
-   digitalWrite(flowsensor, HIGH); // Optional Internal Pull-Up
    Serial.begin(9600);
-   attachInterrupt(0, flow, RISING); // Setup Interrupt
-   sei(); // Enable interrupts
    currentTime = millis();
-   cloopTime = currentTime;
+   previousValue = digitalRead(flowsensor);
+   flow_frequency = 0;
 }
 void loop ()
 {
-   currentTime = millis();
-   // Every second, calculate and print litres/hour
-   if(currentTime >= (cloopTime + 1000))
-   {
-      cloopTime = currentTime; // Updates cloopTime
-//       Pulse frequency (Hz) = 7.5Q, Q is flow rate in L/min.
-Serial.println(flow_frequency);
-      l_hour = (flow_frequency / 7.5); // (Pulse frequency x 60 min) / 7.5Q = flowrate in L/hour
-      flow_frequency = 0; // Reset Counter
-      Serial.print(l_hour, DEC); // Print litres/hour
-      Serial.println(" L/min");
-   }
+  currentValue = digitalRead(flowsensor);
+  if(previousValue == 0 && currentValue == 1)
+  {
+    flow_frequency = flow_frequency + 1;
+  }
+  previousValue = currentValue;
+  Serial.println(flow_frequency);
+  time_lapse= millis() - currentTime;
+  Serial.println(time_lapse);
+  if(time_lapse>= 12500){
+    digitalWrite(sol,LOW);}
+
 }
